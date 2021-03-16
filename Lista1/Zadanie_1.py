22db983f5eaba63fa745553b9b114212c482275f
p_pelar <pawelpelar@gmail.com>
2021-03-16 16:45:56 +0100

    First task
    First task complete, but without comparing with analitical formula (had troubles with it)

--- Lista1/Zadanie_1.py
+++ Lista1/Zadanie_1.py
@@ -18,3 +18,17 @@ def sequence_3(n):
     else:
         return sequence_3(n-1)+sequence_3(n-2)
 
+n=10
+print("Ciąg 1:")
+for i in range(n):
+    print(sequence_1(i))
+
+print("\nCiąg 2:")
+for i in range(n):
+    print(sequence_2(i))
+
+print("\nCiąg 3:")
+for i in range(n):
+    print(sequence_3(i))
+
+
