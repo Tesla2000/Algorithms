7925ce0c3bab1a3a334f7027dae4f37d56b9ff88
Paul <pawelpelar@gmail.com>
2021-04-17 13:02:31 +0200

    Sorted robots towards given param

--- Lista 4/Zadanie_5.py
+++ Lista 4/Zadanie_5.py
@@ -4,17 +4,33 @@ import robot as r
 
 def sorted_identity_vector(r_vector):
     identity = []
+    type = []
+    mass = []
+    range = []
+    resolution = []
 
     for robot in r_vector:
         identity.append(robot.identity)
+        type.append(robot.type)
+        mass.append(robot.mass)
+        range.append(robot.range)
+        resolution.append(robot.resolution)
 
-    identity_index = np.argsort(identity)
+    identity_index = list(np.argsort(identity))
+    type_index = list(np.argsort(type))
+    mass_index = list(np.argsort(mass))
+    range_index = list(np.argsort(range))
+    res_index = list(np.argsort(resolution))
 
-    return identity_index
+    return [identity_index, type_index, mass_index, range_index, res_index]
 
+def sorted_robots(r_vector, sorted_indexes, param):
+    return [r_vector[i] for i in sorted_indexes[param]]
 
 if __name__ == '__main__':
     robots = r.generate_M_robots(10)
     r.print_generated_robots(robots)
 
-    print(sorted_identity_vector(robots))
+    indexes = sorted_identity_vector(robots)
+
+    r.print_generated_robots(sorted_robots(robots, indexes, 4))
