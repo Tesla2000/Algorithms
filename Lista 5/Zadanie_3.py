fea969ac129a6d4ea0cff68ea020a6609f5e58ef
Paul <pawelpelar@gmail.com>
2021-04-26 18:32:36 +0200

    input

--- Lista 5/Zadanie_3.py
+++ Lista 5/Zadanie_3.py
@@ -1,3 +1,5 @@
+from prettytable import PrettyTable
+
 import robot as r
 
 
@@ -36,8 +38,32 @@ def r_list(robot_vector):
     return identity, type_r, mass, range_r, resolution
 
 
+def choose_trait():
+    print('\nChoose trait to be sorted')
+    t = PrettyTable(['CHOOSE:', ''])
+    t.add_row(['1 - ', 'IDENTITY'])
+    t.add_row(['2 - ', 'TYPE'])
+    t.add_row(['3 - ', 'MASS'])
+    t.add_row(['4 - ', 'RANGE'])
+    t.add_row(['5 - ', 'RESOLUTION'])
+    print(t)
+
+    while True:
+        try:
+            number = int(input("Type number: "))
+            if number in [1, 2, 3, 4, 5]:
+                return number - 1
+            else:
+                print('\nInvalid number')
+                print(t)
+        except Exception as err:
+            print(err)
+            print(t)
+
+
 if __name__ == "__main__":
     r_vector = r.generate_M_robots(10)
     r.print_generated_robots(r_vector)
 
-    print(quicksort(r_list(r_vector)[2]))
+    trait = choose_trait()
+    print(f'Sorted in relation to given trait: {quicksort(r_list(r_vector)[trait])}')
